0f322dafd36a324beb9aa730827bc73dfd6d9c45
William22fm <william22fm@gmail.com>
2021-05-26 21:58:43 +0700

    first commit

---
+++ Test Suites/Opr.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Opr</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cfb4659c-bccf-48a1-87aa-9d5e4fd878e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>f5c33177-cd3f-4a34-a53b-812adbb76c47</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case Addition</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>658a6236-12a7-450c-9957-9573dea93540</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case Deduction</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>62635ac9-05ab-47f2-a942-c026c2733e98</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case Division</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>92c93bfe-960e-4f1b-9f6c-ece6d1d2834f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case Multiplication</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
